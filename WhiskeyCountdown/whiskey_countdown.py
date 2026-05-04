7334c946bad5482ffab427275a780b44dad0e8ee
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2026-05-04 13:27:16 -0400

    Create whiskey_countdown.py

---
+++ WhiskeyCountdown/whiskey_countdown.py
@@ -0,0 +1,100 @@
+import datetime
+from os import system
+from time import sleep
+
+class TTACalculations:
+    APPROX_ARRIVAL_DATETIME = datetime.datetime(2026, 5, 15, 17)
+
+    @property
+    def time_to_arrival_datetime(self) -> datetime.timedelta:
+        return self.__class__.APPROX_ARRIVAL_DATETIME - datetime.datetime.now()
+
+    @property
+    def time_to_arrival_days(self) -> int:
+        return self.time_to_arrival_datetime.days
+
+    @property
+    def time_to_arrival_hours(self) -> int:
+        return round(self.time_to_arrival_datetime.seconds // 3600)
+
+    @property
+    def time_to_arrival_minutes(self) -> int:
+        return round(self.time_to_arrival_datetime.seconds % 3600 // 60)
+
+    @property
+    def time_to_arrival_seconds(self):
+        return self.time_to_arrival_datetime.seconds % 60
+
+
+class TAAStrings(TTACalculations):
+    TIME_TO_ARRIVAL_STRING = "Time to arrival: "
+    DAYS = "Days"
+    HOURS = "Hours"
+    MINUTES = "Minutes"
+    SECONDS = "Seconds"
+
+    @property
+    def days_string(self):
+        return f"{self.time_to_arrival_datetime.days} {self.__class__.DAYS}"
+
+    @property
+    def hours_string(self):
+        return f"{self.time_to_arrival_hours} {self.__class__.HOURS}"
+
+    @property
+    def minutes_string(self):
+        return f"{self.time_to_arrival_minutes} {self.__class__.MINUTES}"
+
+    @property
+    def seconds_string(self):
+        return f"{self.time_to_arrival_seconds} {self.__class__.SECONDS}"
+
+    def _days_left_string(self):
+        return (f"{self.TIME_TO_ARRIVAL_STRING}{self.days_string}, "
+                f"{self.hours_string}, {self.minutes_string}, {self.seconds_string}")
+
+    def _no_days_left_string(self):
+        return (f"{self.TIME_TO_ARRIVAL_STRING}{self.hours_string},"
+                f" {self.minutes_string}, {self.seconds_string}")
+
+    def _no_hours_left_string(self):
+        return (f"{self.TIME_TO_ARRIVAL_STRING}{self.minutes_string},"
+                f" {self.seconds_string}")
+
+
+class WhiskeyCountdown(TAAStrings):
+
+    @property
+    def countdown_title_string(self):
+        return f"Counting down to {self.__class__.APPROX_ARRIVAL_DATETIME.ctime()}"
+
+    @property
+    def final_countdown_string(self):
+        return '\n'.join([self.countdown_title_string,
+                          self.countdown_string])
+
+    @property
+    def countdown_string(self):
+        if self.time_to_arrival_days == 0:
+            return self._no_days_left_string()
+        elif self.time_to_arrival_hours == 0:
+            return self._no_hours_left_string()
+        else:
+            return self._days_left_string()
+
+    def run_countdown_timer(self) -> str:
+        while True:
+            print(self.final_countdown_string)
+            sleep(1)
+            system('cls')
+
+    def __str__(self):
+        return self.countdown_string
+
+
+class EarlyWhiskeyCountdown(WhiskeyCountdown):
+    APPROX_ARRIVAL_DATETIME = datetime.datetime(2026, 5, 14, 17)
+
+if __name__ == "__main__":
+    whiskey_countdown = WhiskeyCountdown()
+    whiskey_countdown.run_countdown_timer()
